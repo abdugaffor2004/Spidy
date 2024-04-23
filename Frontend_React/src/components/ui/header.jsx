8b69ae532ff721152fb0be6a9619e36649d0c6b5
GLaDOS23 <karabushhenko_pp_0919@1511.ru>
2024-04-23 17:07:24 +0300

    test

--- Frontend_React/src/components/ui/header.jsx
+++ Frontend_React/src/components/ui/header.jsx
@@ -7,10 +7,6 @@ export function Header(){
         <header>
             <nav className="bg-orange-500 py-2 flex justify-center">
                 <Link className="bg-green-500 mx-3 px-2 py-1 rounded" to={'/NotFound'}> NotFound </Link>
-                <Link className="bg-green-500 mx-3 px-2 py-1 rounded" to={'/NotFound'}> NotFound </Link>
-                <Link className="bg-green-500 mx-3 px-2 py-1 rounded" to={'/NotFound'}> NotFound </Link>
-                <Link className="bg-green-500 mx-3 px-2 py-1 rounded" to={'/NotFound'}> NotFound </Link>
-                <Link className="bg-green-500 mx-3 px-2 py-1 rounded" to={'/NotFound'}> NotFound </Link>
                 <Link className="bg-green-500 mx-3 px-2 py-1 rounded" to={'/'}> Home </Link>
             </nav>
         </header>
