ad0211b16ef1233540f21e0b07f7bd0d1c94e295
abdugaffor2004 <abdugafforlutfillaev14@gmail.com>
2024-05-28 12:56:23 +0300

    Доработал интерфейс

--- Frontend_React/src/pages/Results.jsx
+++ Frontend_React/src/pages/Results.jsx
@@ -5,10 +5,9 @@ import { useLocation, useNavigate } from 'react-router-dom'
 
 function Results() {
 
-// const {state} = useLocation()
+const {state} = useLocation()
 const navigate = useNavigate();
 
-const state = true
 
   useEffect( () =>{
     if(!state){
@@ -21,14 +20,15 @@ const state = true
     return null
   }
 
+  console.log(state)
+
   return (
     <div className='no-scrollbar h-screen overflow-y-scroll'>  {/* Избавляеися от скроллбара. no-scrollbar добавил в tailwind.config -> plugins (это не встроенное свойство) */}
       <Header />
       <div className='py-10 flex flex-col items-center '>
-        <QuestionCard isQuestionWithVariants={true} />
-        <QuestionCard />
-        <QuestionCard />
-        <QuestionCard />
+
+        {Array.from(state.questions).map( (value) => <QuestionCard question={value.question} answer={value.answer} answer_variants={value?.answer_variants} isQuestionsWithVariants={state.isQuestionsWithVariants}/> ) }
+
       </div>
     </div>
   )
